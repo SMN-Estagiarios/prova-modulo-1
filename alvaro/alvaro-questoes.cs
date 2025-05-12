0012cfb3e230e4416af5d847164ce9e86233b679
Alvaro <alvarofernando1324@gmail.com>
2025-05-11 23:42:34 -0300

    feat/alvaro: Adicionando Questões 1, 2 e 3

--- alvaro/alvaro-questoes.cs
+++ alvaro/alvaro-questoes.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+
 
 class Program
 {
@@ -7,6 +10,7 @@ class Program
         int opcao;
         do
         {
+            Console.Clear();
             Console.WriteLine("=== MENU DE QUESTÕES ===");
             Console.WriteLine("1. Soma dos N primeiros números naturais");
             Console.WriteLine("2. Verificar se um número é primo");
@@ -18,13 +22,14 @@ class Program
             switch (opcao)
             {
                 case 1:
-                    Questao1();
+                    SomaNumero();
                     break;
                 case 2:
-                    Questao2();
+                    NumeroPrimo();
                     break;
                 case 3:
-                    Questao3();
+                    NumeroMaiorMenor();
+                    Console.ReadKey();
                     break;
                 case 0:
                     Console.WriteLine("Saindo...");
@@ -39,36 +44,72 @@ class Program
         } while (opcao != 0);
     }
 
-    static void Questao1()
+    static void SomaNumero()
     {
         // TODO: Solicite um número N ao usuário e calcule a soma dos N primeiros números naturais
         Console.Write("Digite um número N: ");
         int n = int.Parse(Console.ReadLine());
+        int soma = 0;
 
-        // O aluno deve implementar a soma
+        if (n >= 0)
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                soma += i;
+            }
+            Console.Write($"A soma é: {soma}");
+        }
+        else
+        {
+            Console.WriteLine($"{n} Não é uma opção válida!");
+        }
     }
 
-    static void Questao2()
+    static void NumeroPrimo()
     {
         // TODO: Solicite um número ao usuário e verifique se ele é primo
         Console.Write("Digite um número para verificar se é primo: ");
         int num = int.Parse(Console.ReadLine());
 
         // O aluno deve verificar se o número é primo
+        int contadorPrimo = 0;
+        for (int i = 1; i <= num; i++)
+        {
+            if (num % i == 0)
+            {
+                contadorPrimo++;
+            }
+        }
+        if (contadorPrimo == 2)
+        {
+            Console.WriteLine($"{num} É Primo!");
+        }
+        else
+        {
+            Console.WriteLine($"{num} Não é Primo!");
+        }
     }
 
-    static void Questao3()
+    static void NumeroMaiorMenor()
     {
+        List<int> numeros = new List<int>();
+
         // Desafio: Solicite N números ao usuário e encontre o maior e o menor deles
+
         Console.Write("Quantos números deseja inserir? ");
         int qtd = int.Parse(Console.ReadLine());
+        int i;
 
-        int maior = int.MinValue;
-        int menor = int.MaxValue;
-
-        // Escreva o que falta aqui
-
-        Console.WriteLine($"Maior número: {maior}");
-        Console.WriteLine($"Menor número: {menor}");
+        for (i = 0; i < qtd; i++)
+        {
+            Console.WriteLine("Digite um número");
+            int num = int.Parse(Console.ReadLine());
+            numeros.Add(num);
+        }
+        if (numeros.Count == qtd)
+        {
+            Console.WriteLine($"Maior número: {numeros.Max()}");
+            Console.WriteLine($"Menor número: {numeros.Min()}");
+        }
     }
 }
